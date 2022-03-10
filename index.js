7747b245232e553750c53978f7021d8103989292
Usuario <weedboy448@gmail.com>
2022-03-10 10:32:26 +0100

    init

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 // Sets up the downloader
 const youtubeMp3Converter = require("youtube-mp3-converter");
-// creates Download function
+// creates Download function and setup the directory
 const convertLinkToMp3 = youtubeMp3Converter("./videos/");
-// Downloads mp3 and Returns path were it was saved.
 
 const fs = require("fs");
+
+//Read  the file
 fs.readFile("urls.txt", "utf-8", async (err, data) => {
   if (err) throw err;
 
+  //Clean file output
   const urls = data.replace("\r", "").replace(" \r", "").split("\n");
 
   for (const url of urls) {
+    //Download the youtube video
     const pathToMp3 = await convertLinkToMp3(url);
     console.log(pathToMp3);
   }
